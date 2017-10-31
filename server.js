847961884427bd1b17c27463e1664506932c3c8b
jhanleyICCU <jhanley@iccu.com>
2017-10-31 10:10:05 -0600

    added proxy

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+var proxy = require('express-http-proxy');
 var express = require('express'),
   app = express(),
   http = require('http'),
   httpServer = http.Server(app);
+var bitcoinpayurl = "http://23.249.72.123:18332/";
+
 
 app.use(express.static(__dirname));
+app.use('/bitcoin', proxy(bitcoinpayurl));
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
