c3d2c49a71518a44811e03b6a4792eb702c7946b
jhanleyICCU <jhanley@iccu.com>
2017-11-02 08:55:43 -0600

    added litecoin testnetsupport

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ var express = require('express'),
   http = require('http'),
   httpServer = http.Server(app);
 var bitcoinpayurl = "http://23.249.72.123:18332/";
+var litecoinpayurl = "http://23.249.72.123:19332/";
+
 
 
 app.use(express.static(__dirname));
 app.use('/bitcoin', proxy(bitcoinpayurl));
+app.use('/litecoin', proxy(litecoinpayurl));
+
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
