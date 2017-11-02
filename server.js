1efc94fe52dd043d5260fbd046deac4829f9814b
jhanleyICCU <jhanley@iccu.com>
2017-11-02 09:59:05 -0600

    added serial port to server side

--- server.js
+++ server.js
@@ -5,7 +5,51 @@ var express = require('express'),
   httpServer = http.Server(app);
 var bitcoinpayurl = "http://23.249.72.123:18332/";
 var litecoinpayurl = "http://23.249.72.123:19332/";
+var currentBalance = 0;
 
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/ttyACM0', {
+  baudRate: 9600
+});
+
+
+// On open to bill accepter
+port.on("open", function () {
+
+    console.log('open');
+
+    port.on('data', function(data) {
+        currentBalance = parseInt(currentBalance) + parseInt(data);
+        console.log(currentBalance);
+        //$("#currentBalance").html(accounting.formatMoney(currentBalance));
+        //balanceUP();
+    });
+});
+
+
+/*
+// serial port reader -- may need to adjust
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+var sp = new SerialPort("/dev/ttyACM0", {
+    baudrate: 9600,
+    parser: serialport.parsers.readline('\r\n')
+});
+
+// On open to bill accepter
+sp.on("open", function () {
+
+    console.log('open');
+
+    sp.on('data', function(data) {
+        currentBalance = parseInt(currentBalance) + parseInt(data);
+        console.log(currentBalance);
+        //$("#currentBalance").html(accounting.formatMoney(currentBalance));
+        //balanceUP();
+    });
+});
+*/
 
 
 app.use(express.static(__dirname));
