116f88ff7854bfa610184bf890cd184468152f35
jhanleyICCU <jhanley@iccu.com>
2017-11-13 09:25:08 -0700

    loopback test

--- server.js
+++ server.js
@@ -8,24 +8,42 @@ var litecoinpayurl = "http://23.249.72.123:19332/";
 var currentBalance = 5;
 
 var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyAMA0', {
-  baudRate: 9600
+var port = new SerialPort('/dev/ttyAMA0', function(err) {
+    if (err) {
+        return console.log('Error: ' + err.message);
+    }
+    baudRate: 9600
+    console.log('open');
+});
+port.write('im a loopback test!');
+
+port.on('data', function(data) {
+    console.log('Data: ' + data);
+    /*
+    currentBalance = parseInt(currentBalance) + parseInt(data);
+    console.log(currentBalance);
+    */
 });
 
-
 // On open to bill accepter
+/*
 port.on("open", function () {
 
     console.log('open');
 
     port.on('data', function(data) {
+        console.log('Data: ' + data);
+        /*
         currentBalance = parseInt(currentBalance) + parseInt(data);
         console.log(currentBalance);
+        */
+        /*
     });
     port.on('error', function(err) {
         console.log('Error: ', err.message);
     });
 });
+*/
 
 app.use(express.static(__dirname));
 app.use('/bitcoin', proxy(bitcoinpayurl));
